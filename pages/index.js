76ff976cd62a65e5759d71d099910a8efe568618
Lucas Simon <lucasimonmendes@hotmail.com>
2024-06-21 14:37:29 -0300

    :sparkles: add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import { Box, Container, Heading } from "@chakra-ui/react"
 
 const Page = () => {
   return(
-  <div>Hello</div>
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack developer based in Brazil!
+      </Box>
+
+      <Box display={{ md: 'flex'}}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Lucas Simon
+          </Heading>
+          <p>Web Developer Fullstack</p>
+
+        </Box>
+
+      </Box>
+    </Container>
   )
 }
 
